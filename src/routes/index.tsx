db5631b991822f7dd4c294b785ec016925adcbe9
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2024-08-06 01:50:47 +0100

    feat: persistent file system permissions

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,29 +1,54 @@
 import { Title } from "@solidjs/meta";
 import { useNavigate } from "@solidjs/router";
-import { createSignal } from "solid-js";
+import { createSignal, onMount } from "solid-js";
 import { useUtensilContext } from "~/components/UtensilProvider";
 import { filterImageHandles, getUtensils, handleToURL } from "~/helpers/files";
 
 export default function Home() {
 	const utensilCtx = useUtensilContext();
 	const navigate = useNavigate();
+	const loadFiles = async (handle: FileSystemDirectoryHandle) => {
+		const vals = handle.values();
+		if (!vals) return;
+		const imageHandles = await filterImageHandles(vals);
+		const utensils = getUtensils(imageHandles);
+		utensilCtx.utensils = utensils;
+		navigate("/rank");
+	};
+	onMount(async () => {
+		// Try to load from IndexedDB
+		const req = indexedDB.open("Db");
+		req.onsuccess = async () => {
+			const items = req.result
+				.transaction("handlers")
+				.objectStore("handlers")
+				.get("handle");
+			items.onsuccess = async () => {
+				const res = items.result as FileSystemDirectoryHandle;
+				if ((await res.requestPermission()) === "granted") {
+					loadFiles(res);
+				}
+			};
+		};
+	});
 	return (
 		<main class="h-full">
 			<Title>Utensily</Title>
 			<div class="h-full flex items-center justify-center">
 				<button
 					type="button"
-          class="rounded-1 bg-blue-400 hover:bg-blue-500 h-20 w-30 mt-100 text-dark-800 border-none"
+					class="rounded-1 bg-blue-400 hover:bg-blue-500 h-20 w-30 mt-100 text-dark-800 border-none"
 					onClick={async () => {
 						const handle = await window.showDirectoryPicker({
 							mode: "readwrite",
 						});
-						const vals = handle.values();
-						if (!vals) return;
-						const imageHandles = await filterImageHandles(vals);
-						const utensils = getUtensils(imageHandles);
-						utensilCtx.utensils = utensils;
-						navigate("/rank");
+						const req = indexedDB.open("Db", 2);
+						req.onupgradeneeded = () => {
+							const db = req.result;
+							const handles = db.createObjectStore("handlers");
+							handles.put(handle, "handle");
+						};
+						loadFiles(handle);
 					}}
 				>
 					Open Directory
